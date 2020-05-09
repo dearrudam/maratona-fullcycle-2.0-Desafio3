da2f50671ba9628930a00fea3d3a43071ea52200
Maximillian Arruda <marruda@opentext.com>
2020-05-09 05:23:16 -0300

    feat: trying to resolve an elint warning

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    sourceType: 'module'    
+    sourceType: 'module',
+    extraFileExtensions: ['.html']
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
