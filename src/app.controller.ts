11b9c8869fa1b2f11bd4a5e448dd0b9f1834612f
Maximillian Arruda <marruda@opentext.com>
2020-05-09 06:44:28 -0300

    refactor: adding hbs template engine

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,23 +1,18 @@
-import { Controller, Get, Redirect, Logger } from '@nestjs/common';
+import { Controller, Get, Redirect, Logger, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  
-  @Get()
-  @Redirect('index.html', 302)
-  goToIndex(): void {
-    Logger.log('redirect to /index.html', AppController.name);
-  }
-  @Get('receiver')
-  @Redirect('receiver.html', 302)
-  goToReceiver(): void {
-    Logger.log('redirect to /receiver.html', AppController.name);
+
+  @Get(['receive', 'receive.html', 'receive.htm'])
+  @Render('receive')
+  goToReceive(): void {
+    Logger.log('rendering views/receive.hbs', AppController.name);
   }
 
-  @Get('send')
-  @Redirect('send.html', 302)
+  @Get(['send', 'send.html', 'send.htm'])
+  @Render('send')
   goToSend(): void {
-    Logger.log('redirect to /send.html', AppController.name);
+    Logger.log('rendering views/send.hbs', AppController.name);
   }
 
 }
